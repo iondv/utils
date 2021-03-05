1644a2b0cac052c69affaee5688a8095614cc6b9
Dan Krasilnikov <krasilneg@yandex.ru>
2021-03-05 21:16:29 +1000

    fix

--- bin/import.js
+++ bin/import.js
@@ -67,7 +67,8 @@ load(path.normalize(path.join(process.cwd(), 'i18n')), null, config.lang)
       ),
       {},
       'boot',
-      ['dbSync', 'metaRepo']
+      ['dbSync', 'metaRepo'],
+      ['application']
     )
   )
   .then(scope => alias(scope, scope.settings.get('di-alias')))
